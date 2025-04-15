657f213b4a7f2d446c89cfbcd4aa884891d510b2
bdgToji <viktorbebek.vb@gmail.com>
2025-04-15 23:57:31 +0200

    Initial commit

---
+++ src/main/java/com/example/ai_spell_check/AiSpellCheckApplication.java
@@ -0,0 +1,13 @@
+package com.example.ai_spell_check;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiSpellCheckApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiSpellCheckApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ai_spell_check/AiSpellCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ai_spell_check;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiSpellCheckApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
